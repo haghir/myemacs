88348bd5895c3ce36d29318d48a0efb0292bfa71
Your Name <you@example.com>
2024-03-05 19:00:27 +0900

    Moves the repository settings to repos.el

--- config.el
+++ config.el
@@ -17,14 +17,6 @@
 ;; Global Settings
 ;; ============================================================================
 
-;; Package
-(package-initialize)
-(setq package-archives
-  '(("gnu"    . "https://elpa.gnu.org/packages/")
-    ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-    ("melpa"  . "https://melpa.org/packages/")
-    ("org"    . "https://orgmode.org/elpa/")))
-
 ;; .emacs.d
 (when load-file-name
   (setq user-emacs-directory
